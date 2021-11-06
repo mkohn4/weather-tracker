57bdb3dbf52df1c16afc6c92d64b9d4f1c58cd14
maxkohn <kohn.max@gmail.com>
2021-11-06 14:19:35 -0400

    added most of the js logic for creating card eements for current and daily forecast and icons + saving citys to local storage as an array and creating buttons from it

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,21 +18,34 @@ THEN I am again presented with current and future conditions for that city
 var searchWeather = document.getElementById('searchBtn');
 var cityCol = document.getElementById('city-col');
 var cityInput = document.getElementById('city');
+var currentWeather = document.getElementById('today-temp');
+var fiveDayForecast = document.getElementById('five-day-forecast');
+var cityHistory = JSON.parse(localStorage.getItem('cityHistory')) || [];
+var btnContainer = document.getElementById('button-container');
 var lat;
 var lng;
+
+var saveHistory = function(city){
+    //check to see if city exists in array currently, if not, add to array
+    if (cityHistory.indexOf(city) === -1) {
+        //add city to city history
+        cityHistory.push(city);
+        //save array to locaL storage
+        localStorage.setItem("cityHistory", JSON.stringify(cityHistory));
+    }
+
+}
 
 
 
 
 var getCurrentWeather = function(city) {
-    //clear input
-
-
-    var recentSearch = document.createElement('button');
-    recentSearch.textContent = city;
-    cityCol.append(recentSearch);
     
-
+         //clear input
+         cityInput.value= '';
+        
+    
+    //get mapquest api call for city name
     var mapquestAPI = 'http://www.mapquestapi.com/geocoding/v1/address?key=p3kcpZvR6puTovHsK3MATQbQcfRAszGA&location='+city;
 
     //fetch latitude and longitude from geocoding mapquest api
@@ -57,6 +70,9 @@ var getCurrentWeather = function(city) {
                     fetch(apiUrl).then(function(response) {
                         if (response.ok) {
                             response.json().then(function(data) {
+                                //clear any results from being in current
+                                currentWeather.innerHTML = '';
+
                                 //if response works for the city, log data
                                 console.log(data);
                                 console.log(data.current);
@@ -64,6 +80,127 @@ var getCurrentWeather = function(city) {
                                 console.log('humidity = '+data.current.humidity);
                                 console.log('wind speed = '+data.current.wind_speed);
                                 console.log('uvi = '+data.current.uvi);
+                                //create card container
+                                var currentWeatherCard = document.createElement('div');
+                                //add card container class
+                                currentWeatherCard.classList.add('card');
+                                //create card img
+                                var currentWeatherCardImg = document.createElement('img');
+                                //add card img class
+                                currentWeatherCardImg.classList.add('card-image-top');
+                                //set current weather card img to icon
+                                currentWeatherCardImg.setAttribute("src", "http://openweathermap.org/img/wn/"+data.current.weather[0].icon+".png")
+                                //create card body
+                                var currentWeatherCardBody = document.createElement('div');
+                                //add card body class
+                                currentWeatherCardBody.classList.add('card-body');
+                                //create title
+                                var currentWeatherCardTitle = document.createElement('h5')
+                                //add title class
+                                currentWeatherCardTitle.classList.add('card-title');
+                                //set title = city name
+                                currentWeatherCardTitle.textContent = city;
+                                //create text card
+                                var currentWeatherCardText = document.createElement('div');
+                                //add card text class
+                                currentWeatherCardText.classList.add('card-text');
+                                //create ordered list for data
+                                var currentWeatherCardOL = document.createElement('ul');
+                                //create temp li
+                                var currentWeatherCardTemp = document.createElement('li');
+                                //assign temp li text
+                                currentWeatherCardTemp.textContent = 'Current Temperature (f) = '+data.current.temp;
+                                //create humidity element
+                                var currentWeatherHumidity = document.createElement('li');
+                                //add content to humidity
+                                currentWeatherHumidity.textContent = 'Current Humidity = '+data.current.humidity;
+                                //create list item for current weather wind
+                                var currentWeatherCardWind = document.createElement('li');
+                                currentWeatherCardWind.textContent = 'Current Wind Speed (mph) = '+data.current.wind_speed;
+                                //create list item for uvi
+                                var currentWeatherCardUVI = document.createElement('li');
+                                currentWeatherCardUVI.textContent = 'Current UVI = '+data.current.uvi;
+                                //add card body to card
+                                currentWeatherCard.append(currentWeatherCardBody);
+                                //add image to card
+                                currentWeatherCard.append(currentWeatherCardImg);
+                                //add card title to card body
+                                currentWeatherCardBody.append(currentWeatherCardTitle);
+                                //add card text to body
+                                currentWeatherCardBody.append(currentWeatherCardText);
+                                //add ol to text container
+                                currentWeatherCardText.append(currentWeatherCardOL);
+                                //add list items to ol
+                                currentWeatherCardOL.append(currentWeatherCardTemp);
+                                currentWeatherCardOL.append(currentWeatherHumidity);
+                                currentWeatherCardOL.append(currentWeatherCardWind);
+                                currentWeatherCardOL.append(currentWeatherCardUVI);
+                                //add card to current weather container
+                                currentWeather.append(currentWeatherCard);
+
+                                //clear 5 day forecast
+                                fiveDayForecast.innerHTML = '';
+                            //create 5 elements for daily forecase
+                            for (var i=0; i<5; i++) {
+                                //create card container
+                                var dailyWeatherCard = document.createElement('div');
+                                //add card container class
+                                dailyWeatherCard.classList.add('card');
+                                 //create card img
+                                 var dailyWeatherCardImg = document.createElement('img');
+                                 //add card img class
+                                 dailyWeatherCardImg.classList.add('card-image-top');
+                                 //set current weather card img to icon
+                                 dailyWeatherCardImg.setAttribute("src", "http://openweathermap.org/img/wn/"+data.daily[i].weather[0].icon+".png")
+                                //create card body
+                                var dailyWeatherCardBody = document.createElement('div');
+                                //add card body class
+                                dailyWeatherCardBody.classList.add('card-body');
+                                //create title
+                                var dailyWeatherCardTitle = document.createElement('h5')
+                                //add title class
+                                dailyWeatherCardTitle.classList.add('card-title');
+                                //set title = city name
+                                dailyWeatherCardTitle.textContent = city;
+                                //create text card
+                                var dailyWeatherCardText = document.createElement('div');
+                                //add card text class
+                                dailyWeatherCardText.classList.add('card-text');
+                                //create ordered list for data
+                                var dailyWeatherCardOL = document.createElement('ul');
+                                //create temp li
+                                var dailyWeatherCardTemp = document.createElement('li');
+                                //assign temp li text
+                                dailyWeatherCardTemp.textContent = 'Current Temperature (f) = '+data.daily[i].temp.day;
+                                //create humidity element
+                                var dailyWeatherHumidity = document.createElement('li');
+                                //add content to humidity
+                                dailyWeatherHumidity.textContent = 'Current Humidity = '+data.daily[i].humidity;
+                                //create list item for current weather wind
+                                var dailyWeatherCardWind = document.createElement('li');
+                                dailyWeatherCardWind.textContent = 'Current Wind Speed (mph) = '+data.daily[i].wind_speed;
+                                //create list item for uvi
+                                var dailyWeatherCardUVI = document.createElement('li');
+                                dailyWeatherCardUVI.textContent = 'Current UVI = '+data.daily[i].uvi;
+                                //add card body to card
+                                dailyWeatherCard.append(dailyWeatherCardBody);
+                                dailyWeatherCard.append(dailyWeatherCardImg);
+                                //add card title to card body
+                                dailyWeatherCardBody.append(dailyWeatherCardTitle);
+                                //add card text to body
+                                dailyWeatherCardBody.append(dailyWeatherCardText);
+                                //add ol to text container
+                                dailyWeatherCardText.append(dailyWeatherCardOL);
+                                //add list items to ol
+                                dailyWeatherCardOL.append(dailyWeatherCardTemp);
+                                dailyWeatherCardOL.append(dailyWeatherHumidity);
+                                dailyWeatherCardOL.append(dailyWeatherCardWind);
+                                dailyWeatherCardOL.append(dailyWeatherCardUVI);
+                                //append daily card to daily card container
+                                fiveDayForecast.append(dailyWeatherCard);
+                            }
+                            
+
                                 
                             })
                         } else {
@@ -79,6 +216,27 @@ var getCurrentWeather = function(city) {
     
 };
 
+var inputBtnValue = function(event) {
+    //get button weather value and run search
+    getCurrentWeather(event.target.textContent);
+
+}
+
+var historyBtn = function() {
+    btnContainer.innerHTML = '';
+    
+    for (var i=0; i<cityHistory.length; i++) {
+    //create button
+     var recentSearch = document.createElement('button');
+     //set button text to name of input value
+     recentSearch.textContent = cityHistory[i];
+     recentSearch.addEventListener('click', inputBtnValue);
+     //add button underneath searchbox container
+     btnContainer.append(recentSearch);
+    }
+     
+}
+
 var getCityInput = function(event) {
     //stop page from refreshing
     event.preventDefault();
@@ -86,9 +244,13 @@ var getCityInput = function(event) {
     var city = cityInput.value;
     //run getCurrentWeather function for city
     getCurrentWeather(city);
-
+    saveHistory(city);
+    //call historyBtn
+    historyBtn();
 }
 
 
 //on click of Get Weather button, run get city input function
 searchWeather.addEventListener("click", getCityInput)
+//call historyBtn
+historyBtn();
